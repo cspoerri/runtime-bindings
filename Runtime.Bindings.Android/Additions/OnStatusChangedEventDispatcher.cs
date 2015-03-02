43f18d2cd36c7552770615f7a367cd089e182673
spc <>
2015-03-02 23:11:42 +0100

    * OnStatusChangedEventDispatcher.cs: added event handler for   StatusChanged event on mapview (Note: this was not added   automatically since it contains multiple event arguments (see also   http://developer.xamarin.com/guides/android/advanced_topics/api_design/   section on "Events and Listeners")
    * Runtime.Bindings.Android.csproj: changed target framework to android
      4.3
    
    * Metadata.xml: removed some nodes, which don't have a visibility set
      in the automatic transformation, which lead to issues in using the
      bindings

---
+++ Runtime.Bindings.Android/Additions/OnStatusChangedEventDispatcher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Java.Nio;
+using Com.Esri.Android.Map.Event;
+
+namespace Com.Esri.Android.Map
+{
+
+	internal partial class OnStatusChangedEventDispatcher : Java.Lang.Object, IOnStatusChangedListener
+	{
+		MapView sender;
+
+		public OnStatusChangedEventDispatcher(MapView sender)
+		{
+			this.sender = sender;
+		}
+
+		internal EventHandler<StatusChangedEventArgs> StatusChanged;
+
+		public void OnStatusChanged (Java.Lang.Object sender, Com.Esri.Android.Map.Event.OnStatusChangedListenerSTATUS status)
+		{
+			var h = StatusChanged;
+			if (h != null)
+				h(sender, new StatusChangedEventArgs() { Status = status });
+		}
+	}
+
+	public class StatusChangedEventArgs : EventArgs
+	{
+		public OnStatusChangedListenerSTATUS Status { get; internal set; }
+	}
+
+	public partial class MapView
+	{
+		private WeakReference statusChangedDispatcher;
+
+		private OnStatusChangedEventDispatcher StatusChangedEventDispatcher
+		{
+			get
+			{
+				if (statusChangedDispatcher == null || !statusChangedDispatcher.IsAlive)
+				{
+					var d = new	OnStatusChangedEventDispatcher(this);
+					OnStatusChangedListener = d;
+					statusChangedDispatcher = new WeakReference(d);
+				}
+				return(OnStatusChangedEventDispatcher)	statusChangedDispatcher.Target;
+			}
+		}
+
+		public event EventHandler<StatusChangedEventArgs> StatusChanged
+		{
+			add
+			{
+				StatusChangedEventDispatcher.StatusChanged += value;
+			}
+
+			remove
+			{
+				StatusChangedEventDispatcher.StatusChanged -= value;
+			}
+		}
+	}
+}
